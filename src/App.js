89f251cda028a91e57ddcd6fc7865ee7bea8c0fc
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2023-02-10 17:07:55 +0530

    src folder uploaded

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Clock from './components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>BREAK REMINDER</h1>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
